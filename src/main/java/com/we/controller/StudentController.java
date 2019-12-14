ca0ec66e767734c8e988295bbd0efefadafcbb4e
aman.ullah <aman.ullah@bracits.com>
2019-12-14 19:56:31 +0600

    report exportmode mod

--- src/main/java/com/we/controller/StudentController.java
+++ src/main/java/com/we/controller/StudentController.java
@@ -8,8 +8,13 @@ import com.we.services.action.studentinfo.GenerateRptStudentInfoActionService;
 import com.we.services.action.studentinfo.ListStudentInfoActionService;
 import com.we.services.action.studentinfo.SelectStudentInfoActionService;
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
+import net.sf.jasperreports.export.SimpleXlsReportConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -112,15 +117,35 @@ public class StudentController extends BaseController {
     public void export(@RequestParam Map<String, Object> parameters, ModelAndView model, HttpServletResponse response) throws IOException, JRException, SQLException {
         JasperPrint jasperPrint = null;
         OutputStream out = response.getOutputStream();
-
         /*response.setHeader("Content-Disposition", String.format("attachment; filename=\"Demo_Report.pdf\""));
         response.setContentType("application/x-download");*/
-
         response.setHeader("Content-Disposition", String.format("inline; filename=Demo_Report" + "_" + new Date() + ".pdf"));
         response.setContentType("application/pdf");
 
         jasperPrint = generateRptStudentInfoActionService.exportPdfFile(parameters);
-        JasperExportManager.exportReportToPdfStream(jasperPrint, out);
+        this.exportModeSelector("pdf", jasperPrint, out);
 
+    }
+
+    private void exportModeSelector(String xprtMode, JasperPrint jasperPrint, OutputStream outputStream) throws JRException {
+        if (xprtMode.equals("pdf")) {// exports report to pdf
+            JRPdfExporter exporter = new JRPdfExporter();
+            exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
+            SimplePdfExporterConfiguration configuration = new SimplePdfExporterConfiguration();
+            exporter.setConfiguration(configuration);
+            exporter.exportReport();
+        } else {// exports report to excel
+            JRXlsExporter xlsExporter = new JRXlsExporter();
+            xlsExporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+            xlsExporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
+            SimpleXlsReportConfiguration xlsReportConfiguration = new SimpleXlsReportConfiguration();
+            xlsReportConfiguration.setOnePagePerSheet(false);
+            xlsReportConfiguration.setRemoveEmptySpaceBetweenRows(true);
+            xlsReportConfiguration.setDetectCellType(true);
+            xlsReportConfiguration.setWhitePageBackground(false);
+            xlsExporter.setConfiguration(xlsReportConfiguration);
+            xlsExporter.exportReport();
+        }
     }
 }
--- src/main/java/com/we/services/action/studentinfo/GenerateRptStudentInfoActionService.java
+++ src/main/java/com/we/services/action/studentinfo/GenerateRptStudentInfoActionService.java
@@ -2,13 +2,6 @@ package com.we.services.action.studentinfo;
 
 import com.we.services.BaseService;
 import net.sf.jasperreports.engine.*;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.export.JRPdfExporter;
-import net.sf.jasperreports.engine.export.JRXlsExporter;
-import net.sf.jasperreports.export.SimpleExporterInput;
-import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
-import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
-import net.sf.jasperreports.export.SimpleXlsReportConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +9,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Service
@@ -32,21 +22,19 @@ public class GenerateRptStudentInfoActionService extends BaseService {
     DataSource dataSource;
 
     public JasperPrint exportPdfFile(Map params) throws SQLException, JRException, IOException {
+        Map<String, Object> parameters = new HashMap<String, Object>();
 
         String reportPath = "D:\\Rpt";
-        List<String> empList = new ArrayList<>();
         // Compile the Jasper report from .jrxml to .japser
         JasperReport jasperReport = JasperCompileManager.compileReport(reportPath + "\\SpringBootTest1.jrxml");
-        JRBeanCollectionDataSource jrBeanCollectionDataSource = new JRBeanCollectionDataSource(empList);
-
         // Parameters for report
-        Map<String, Object> parameters = new HashMap<String, Object>();
 
         JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, dataSource.getConnection());
 
         return print;
     }
 
+/*
     Map execute(Map previousResult) {
         String jasperFileName, subReportFilePath, mainJasperFileName, subReportSubsidiaryFilePath, subReportName, reportPath = "";
         String PDF_STREAM = "pdfStream";
@@ -90,9 +78,12 @@ public class GenerateRptStudentInfoActionService extends BaseService {
             log.error(ex.getMessage());
             throw new RuntimeException(ex);
         }
-        /*finally {
+        */
+/*finally {
             pdfStream.close();
-        }*/
+        }*//*
+
     }
+*/
 
 }
